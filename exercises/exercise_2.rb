cf0d1ab159300a0344e181ce30c8da0f0009929a
Hatem <hatem.deux@gmail.com>
2020-04-28 18:27:42 -0400

    fixes small issue in  Exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,13 +6,13 @@ puts "----------"
 
 # Your code goes here ...
 
-store1 = Store.find_by(id: 1)
-store2 = Store.find_by(id: 2)
-puts "store 1 is : "
-puts store1.name
-puts "store 2 is : "
-puts store2.name
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+puts "@store 1 is : "
+puts @store1.name
+puts "@store 2 is : "
+puts @store2.name
 
-store1.update(name: 'Burnaby_NEW')
-puts "store 1 now is : "
-puts store1.name
+@store1.update(name: 'Burnaby_NEW')
+puts "@store 1 now is : "
+puts @store1.name
