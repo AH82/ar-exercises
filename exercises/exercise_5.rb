4bc55f8074270c5783ab45d1fb656bdc8d887b87
Hatem <hatem.deux@gmail.com>
2020-04-28 19:26:14 -0400

    implements Exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "SUM annual_revenue for all stores : #{Store.sum(:annual_revenue)}"
+puts "AVG annual_revenue for all stores : #{Store.average(:annual_revenue)}"
+puts "count of >= 1M :"
+puts @stores_over_1M = Store.where("annual_revenue >= 1000000").count # or .size
