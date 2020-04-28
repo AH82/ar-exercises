75b20da9dac8cd6084fa528f947febf473e39311
Hatem <hatem.deux@gmail.com>
2020-04-28 18:10:34 -0400

    implements Exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+store1 = Store.find_by(id: 1)
+store2 = Store.find_by(id: 2)
+puts "store 1 is "
+puts store1.name
+puts "store 2 is : "
+puts store2.name
+
+store1.update(name: 'Richmond_NEW')
+puts "store 1 now is "
+puts store1.name
